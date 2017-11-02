b17a80cd88617d22b59df56354dcc4a3267fa1ae
김영현 <yh.kim@aonkorea.com>
2017-11-02 10:25:14 +0900

    관리자 시큐리티 설정 및 테스트용 컨트롤러

--- src/main/java/com/example/conf/WebSecurityConfig.java
+++ src/main/java/com/example/conf/WebSecurityConfig.java
@@ -1,18 +1,37 @@
 package com.example.conf;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/*", "/login").permitAll();
-		http.authorizeRequests().anyRequest().authenticated();
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("user").password("user").roles("USER");
+		auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN");
+	}
 
-//		super.configure(http);
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/resources/**");
+	}
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		// public
+		http.authorizeRequests().antMatchers("/", "/index.abc").permitAll();
+
+		// admin
+		http.authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN");
+
+		// user
+		http.authorizeRequests().antMatchers("/**").hasRole("USER");
+
+		http.formLogin().defaultSuccessUrl("/").and().logout().logoutUrl("/logout").logoutSuccessUrl("/");
 	}
 
 }
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -1,10 +1,20 @@
 <%@page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <html>
 <head>
 </head>
 <body>
 	<h1>${msg}</h1>
 	<br>
-	<h2>test</h2>
+	<c:if test="${pageContext.request.userPrincipal.name == null}">
+		<a href="/login">login</a>
+	</c:if>
+	
+	<c:if test="${pageContext.request.userPrincipal.name != null}">
+		<form action="/logout" method="post">
+			<input type="hidden" name="${_csrf.parameterName}" value="${_csrf.token}" />
+			<input type="submit" value="logout">
+		</form>
+	</c:if>
 </body>
 </html>
