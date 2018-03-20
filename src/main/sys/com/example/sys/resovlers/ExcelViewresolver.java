f9beb60f93c1b8cf8269ecb7bf618d2953150d76
kyh <kyh@DESKTOP-7A5S7QR>
2018-03-20 12:36:05 +0900

    excel view 추가.

---
+++ src/main/sys/com/example/sys/resovlers/ExcelViewresolver.java
@@ -0,0 +1,15 @@
+package com.example.sys.resovlers;
+
+import java.util.Locale;
+
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
+
+public class ExcelViewresolver implements ViewResolver {
+
+	@Override
+	public View resolveViewName(String viewName, Locale locale) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
